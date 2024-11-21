41c50ef5683e6f398f329f5213d4de39e98ba21c
Ghritachi Mahajani <ghritachi.mahajani@gmail.com>
2024-11-20 17:07:23 -0700

    Ghritachi - added more conditions

--- api_gateway/server.py
+++ api_gateway/server.py
@@ -3,24 +3,28 @@ from flask import Flask, request, Response
 from werkzeug.utils import secure_filename
 import jsonpickle
 
+# Temporary folder for keeping uploads ( can remove later if we want to send it immediately.)
 UPLOAD_FOLDER = 'uploads'
+
+# Change the allowed extensions if needed
 ALLOWED_EXTENSIONS = {'txt', 'csv',}
+
+# Creating app
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Create a temporary folder to keep uploaded log files if not already there.
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
        os.makedirs(app.config['UPLOAD_FOLDER'])
 
+# check if the extension of the file is in allowed list
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/apiv1/generate-insight', methods=['POST'])
 def processLog():
-    # data = request.get_json()
-    # hash = mc.addmp3Tobucket(mc.ipbucket,data['mp3'])
-    # rc.enqueueWork(item=hash)
-    
+
     response = {}
     if request.method == 'POST':
         # check if the post request has the file part
@@ -30,9 +34,8 @@ def processLog():
             status=400
         
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
 
+        # if an empty? file exists ( not sure how that will be)
         if file.filename == '':
             response={"msg": "Error: 'Invalid File"}
             status=400
@@ -43,6 +46,11 @@ def processLog():
             # return redirect(url_for('download_file', name=filename))
             response = {"msg":"Success! file received successfully"}
             status=200
+        # either file does not exist or is not the extension allowed.
+        else:
+            response={"msg": "Error: 'Invalid File"}
+            status=400
+
 
     response_pickled = jsonpickle.encode(response)
     return Response(response=response_pickled, status=status, mimetype="application/json")
